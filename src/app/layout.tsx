52ef53a67aa0c5013bf53f9d9b86f8032d0095f8
00Danii <jdaniel.om97@gmail.com>
2025-06-09 15:14:17 -0600

    cambio de imagen og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: "00Danii",
     images: [
       {
-        url: "https://i.imgur.com/t7SlXqe.png",
+        url: "https://i.imgur.com/UbpWM1h.png",
         // width: 1200,
         // height: 630,
         alt: "Ejemplo de arte ASCII",
