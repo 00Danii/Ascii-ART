a3eda38e40e869756cfcd58a7fe51d72c37188aa
00Danii <jdaniel.om97@gmail.com>
2025-06-08 19:55:40 -0600

    quitar arte que no sirve xd

--- src/constants/ascii.ts
+++ src/constants/ascii.ts
@@ -5,7 +5,15 @@ export const ASCII_CHARS = [
   " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$",
   " .,-~:;=!*#$@",
   " ░▒▓█",
-  " ⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿",
+  // " ⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿",
+  // " ⠀⠁⠈⠂⠊⠒⠢⠆⠤⠔⠴⠌⠬⠼⠽⠿", // Braille (optimizado)
+  // " ◦•◘○●◉⬢⬣⬡", // Círculos y Formas
+  " ┌┐└┘│─┼┤├┬┴╭╮╯╰║═╬╣╠╦╩", // Líneas y Marcos
+  // " ∙∘○◯●◉⚫⚪", // Puntos Geométricos
+  // " ▁▂▃▄▅▆▇█", // Barras Verticales
+  " ╱╲╳▲▼◄►♦♠♣♥", // Símbolos Especiales
+  // " ⟨⟩⟪⟫⟬⟭⟮⟯⟰⟱", // Matemáticos
+  " ░▒▓█▀▄▌▐■□▪▫", // Bloques Avanzados
 ];
 
 export const CHAR_SET_NAMES = [
@@ -13,7 +21,15 @@ export const CHAR_SET_NAMES = [
   "Detallado",
   "Minimalista",
   "Bloques",
-  "Braille",
+  // "Braille",
+  // "Braille Optimizado",
+  // "Formas Geométricas",
+  "Líneas y Marcos",
+  // "Puntos Modernos",
+  // "Barras Graduales",
+  "Símbolos Especiales",
+  // "Matemático",
+  "Bloques Avanzados",
 ];
 
 export const COLOR_OPTIONS: ColorOption[] = [
@@ -23,5 +39,8 @@ export const COLOR_OPTIONS: ColorOption[] = [
   { name: "Gris", value: "text-gray-300", bg: "bg-black" },
   { name: "Cyan", value: "text-cyan-400", bg: "bg-black" },
   { name: "Amarillo", value: "text-yellow-400", bg: "bg-black" },
+  { name: "Azul", value: "text-blue-500", bg: "bg-black" },
+  { name: "Púrpura", value: "text-purple-500", bg: "bg-black" },
+  { name: "Naranja", value: "text-orange-500", bg: "bg-black" },
   { name: "Tonos Originales", value: "original", bg: "bg-black" },
 ];
