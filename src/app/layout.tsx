1752368be3a043965e4ff76858c8d6ce522bc7a3
00Danii <jdaniel.om97@gmail.com>
2025-06-23 19:33:59 -0600

    se cambio la og image de la pagina

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: "00Danii",
     images: [
       {
-        url: "https://i.imgur.com/UbpWM1h.png",
+        url: "https://i.imgur.com/vy3QQCF.jpeg",
         // width: 1200,
         // height: 630,
         alt: "Ejemplo de arte ASCII",
